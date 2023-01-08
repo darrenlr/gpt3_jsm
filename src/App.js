6f8cd3f5e93542924c0f517089137b6289e19f07
Darren Russell <darren.russell@erudus.com>
2023-01-08 17:29:11 +0000

    add css styling

--- src/App.js
+++ src/App.js
@@ -9,11 +9,13 @@ import {
 	Header,
 } from "./containers";
 import { Brand, CTA, Navbar } from "./components";
+import "./App.css";
+import "./index.css";
 
 const App = () => {
 	return (
 		<div className="App">
-			<div className="gradients__bg">
+			<div className="gradient__bg">
 				<Navbar />
 				<Header />
 			</div>
