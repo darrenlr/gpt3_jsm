02223bc7913879cabc8ab5b28180ac98f5b52083
Darren Russell <darren.russell@erudus.com>
2023-01-21 18:37:13 +0000

    implment and style Possibility section

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./possibility.css";
+import possibilityImage from "../../assets/possibility.png";
 
 const Possibility = () => {
-	return <div>Possibility</div>;
+	return (
+		<div className="gpt3__possibility section__padding" id="possibility">
+			<div className="gpt3__possibility-image">
+				<img src={possibilityImage} alt="possibility" />
+			</div>
+			<div className="gpt3__possibility-content">
+				<h4>Request Early Access to Get Started</h4>
+				<h1 className="gradient__text">
+					The possibilities are only limited by your imagination
+				</h1>
+				<p>
+					Lorem Ipsum has been the industry's standard dummy text ever since the
+					1500s, when an unknown printer took a galley of type and scrambled it
+					to make a type specimen book.
+				</p>
+				<h4>Request Early Access to Get Started</h4>
+			</div>
+		</div>
+	);
 };
 
 export default Possibility;
