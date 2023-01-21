c1ab11dcc15cb8cca0a26f42203dd8a4aff3045d
Darren Russell <darren.russell@erudus.com>
2023-01-21 21:27:12 +0000

    implement and style footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,8 +1,46 @@
 import React from "react";
 import "./footer.css";
+import gpt3Logo from "../../assets/logo.svg";
 
 const Footer = () => {
-	return <div>Footer</div>;
+	return (
+		<div className="gpt3__footer section__padding">
+			<div className="gpt3__footer-heading">
+				<h1 className="gradient__text">Do you want to step into the future?</h1>
+			</div>
+			<div className="gpt3__footer-btn">
+				<p>Request Early Access</p>
+			</div>
+
+			<div className="gpt3__footer-links">
+				<div className="gpt3__footer-links_logo">
+					<img src={gpt3Logo} alt="logo" />
+					<p>1 Madeup Street Silent Hill NN32 5ST, All Rights Reserved</p>
+				</div>
+				<div className="gpt3__footer-links_div">
+					<h4>Links</h4>
+					<p>Overons</p>
+					<p>Social Media</p>
+					<p>Counters</p>
+				</div>
+				<div className="gpt3__footer-links_div">
+					<h4>Company</h4>
+					<p>Terms & Conditions</p>
+					<p>Privacy Policy</p>
+					<p>Contact</p>
+				</div>
+				<div className="gpt3__footer-links_div">
+					<h4>Get in touch</h4>
+					<p>1 Madeup Street Silent Hill NN32 5ST</p>
+					<p>01348 267546</p>
+					<p>info@payme.net</p>
+				</div>
+			</div>
+			<div className="gpt3__footer-copyright">
+				<p>© 2023 GPT-3. All rights reserved.</p>
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
